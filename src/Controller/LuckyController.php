ffd532f2900c1f985b9db9a5a2b4f3f0dc58bd63
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-07-10 00:46:05 +0300

    update

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,6 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class LuckyController
+ *
+ * @package App\Controller
+ *
+ * @Route("/blog", requirements={"_locale": "en|es|fr"}, name="blog_")
+ */
 class LuckyController extends AbstractController
 {
     /**
@@ -40,6 +47,59 @@ class LuckyController extends AbstractController
     {
         return new Response(
             '<html><head><title>Show lucky slug</title></head> <body><p>Lucky slug: ' . $slug . '</p></body></html>'
+        );
+    }
+
+    /**
+     * @Route(
+     *     "/lucky/search",
+     *     locale="en",
+     *     format="html",
+     *     priority=2,
+     *     defaults={
+     *          "query": "default value",
+     *     },
+     *     requirements={
+     *          "_locale": "en|fr",
+     *          "_format": "html|xml",
+     *     }
+     * )
+     * @param string $query
+     * @return Response
+     */
+    public function search(string $query)
+    {
+        return new Response(
+            'Your search query: ' . $query
+        );
+    }
+
+    /**
+     * @Route({
+     *     "en": "/about-us",
+     *     "nl": "/over-ons"
+     * }, name="about_us")
+     * @return Response
+     */
+    public function about()
+    {
+        return new Response(
+            '<html><head><title>about</title></head><body>my about page</body></html>'
+        );
+    }
+
+    /**
+     * @Route(
+     *     "/",
+     *     name="homepage",
+     *     stateless=true
+     * )
+     * @return Response
+     */
+    public function homepage()
+    {
+        return new Response(
+            'hello from homepage without caching'
         );
     }
 }
